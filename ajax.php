d1ca4f3e9ab36415635d55c87b5a20cb907750f6
Ivo Fokkema <Ivo@IFokkema.nl>
2024-07-23 18:49:06 +0200

    Format errors properly and improve the current errors.
    Also, require the completion of the configuration before using the
     app - without it, we can't connect.

--- ajax.php
+++ ajax.php
@@ -7,6 +7,11 @@ $a = [
     'data' => [],
 ];
 
+$_CONFIG = @json_decode(file_get_contents('settings.json'), true);
+if (count(array_intersect_key($_CONFIG, array_flip(['api_key', 'model', 'assistant']))) < 3) {
+    die(json_encode(array_merge($a, ['errors' => ['ENOCONFIG' => 'Configuration of this app was not complete. Run the configuration first.']])));
+}
+
 if (empty($_POST['input'])) {
     die(json_encode(array_merge($a, ['errors' => ['ENODATA' => 'No data was received.']])));
 }
